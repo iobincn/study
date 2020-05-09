d6295a06bd849ddfb5dabc3d6546185754e2f79a
HSM <iobincn@gmail.com>
2020-05-09 20:56:37 +0800

    update

--- DUIBI.php
+++
@@ -1,9 +0,0 @@
-<?php
-$sql="INSERT INTO Persons (FirstName, LastName, Age)
-VALUES
-('$_POST[firstname]','$_POST[lastname]','$_POST[age]')";
-
-$sql = "INSERT INTO student(name,pic,course,grade) VALUES($name,$pic,$course,$grade)";
-
-
-?>
